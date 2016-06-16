292d926da22cc58e708a244a9cf6164d10f1be39
Brandon Long <blong1996@gmail.com>
2016-06-15 21:05:28 -0400

    Assignment Five
    Problem One nearly complete,
    Start Problem Two

---
+++ Assignment Five/js/problem-one.js
@@ -0,0 +1,75 @@
+/**
+ * Brandon Long
+ * June 16, 2016
+ * Internet Systems
+ * Assignment Five  | Problem One
+ *
+ * /\\\\\\\\\\\\    /\\\              /\\\\\\\\\\\    /\\\\\     /\\\    /\\\\\\\\\\\
+ *  \/\\\////////\\\ \/\\\            /\\\\////////\\\ \/\\\\\\   \/\\\  /\\\\////////\\\
+ *   \/\\\      \/\\\ \/\\\           \/\\\       \/\\\ \/\\\/\\\  \/\\\ \/\\\       \///
+ *    \/\\\\\\\\\\\\/  \/\\\           \/\\\       \/\\\ \/\\\//\\\ \/\\\ \/\\\
+ *     \/\\\///////\\\\ \/\\\           \/\\\       \/\\\ \/\\\\//\\\\/\\\ \/\\\     /\\\\\\
+ *      \/\\\      \/\\\ \/\\\           \/\\\       \/\\\ \/\\\ \//\\\/\\\ \/\\\    \////\\\
+ *       \/\\\      \/\\\ \/\\\           \/\\\       \/\\\ \/\\\  \//\\\\\\ \/\\\       \/\\\
+ *        \/\\\\\\\\\\\\/  \/\\\\\\\\\\\\\ \/\/\\\\\\\\\\\/  \/\\\   \//\\\\\ \/\/\\\\\\\\\\\/
+ *         \////////////    \/////////////    \///////////    \///     \/////    \///////////
+ */
+
+function start()  {
+    document.getElementById('tr_no').click();
+    document.getElementById('sm_no').click();
+    document.forms[0].dayexp.addEventListener( 'blur', checkCurrency, false );
+    document.forms[0].tr_yes.addEventListener( 'click', fixTravel, false );
+    document.forms[0].tr_no.addEventListener( 'click', fixTravel, false );
+    document.forms[0].sm_yes.addEventListener( 'click', fixMeeting, false );
+    document.forms[0].sm_no.addEventListener( 'click', fixMeeting, false );
+    document.forms[0].iden.addEventListener( 'blur', checkIDall, false );
+}
+
+function checkCurrency(e) {
+    // check if input is valid and has a $ sign
+    var val = e.target.value;
+    var regEx = /^\$?[0-9][0-9,]*[0-9]\.?[0-9]{0,2}$/i;
+
+    if (!regEx.test(val)) {
+        window.alert(val + " is not a valid input");
+    }
+    else {
+        if(val[0] != "$") {
+            e.reset();
+        }
+    }
+}
+
+function fixTravel(e) {
+    // if yes, show travel box, else, hide it
+    if(e.target.value == "yes") {
+        document.getElementById("trbox").setAttribute("style", "display:block");
+        document.forms[0].trexp.addEventListener( 'blur', checkCurrency, false );
+    } else {
+        document.getElementById("trbox").setAttribute("style", "display:none");
+    }
+}
+
+function fixMeeting(e) {
+    // if yes, show meeting box, else hide it
+    if(e.target.value == "yes") {
+        document.getElementById("smbox").setAttribute("style", "display:block");
+        document.forms[0].smfees.addEventListener( 'blur', checkCurrency, false );
+    } else {
+        document.getElementById("smbox").setAttribute("style", "display:none");
+    }
+}
+
+function checkIDall(e) {
+    // check if ID is valid based on regular expression
+    var regExp = /^[A-Z]\d{3}$/;
+    var val = e.target.value;
+    var ID = document.getElementById("iden");
+
+    if (!regExp.test(val)) {
+        window.alert("Wrong ID format.");
+        ID.reset();
+    }
+
+}
